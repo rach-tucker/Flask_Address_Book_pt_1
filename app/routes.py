acce27b499bf58817776906706480f7eac025a4c
Rachel Tucker <rach.mello@hotmail.com>
2023-01-11 19:52:07 -0600

    first commit

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from app import app
 from flask import render_template, redirect, url_for, flash
 from app.forms import SignUpForm
+from app.models import User
 
 @app.route('/')
 def index():
@@ -17,11 +18,20 @@ def signup():
         username = form.username.data
         password = form.password.data
         print(first_name, last_name, email, username, password)
-
-        flash('Thank you {{ first_name }} {{ last_name }} for signing up.')
-
+        # Query our user table to see if there are any users with either username or email from form
+        check_user = User.query.filter( (User.username == username) | (User.email == email) ).all()
+        # If the query comes back with any results
+        if check_user:
+            # Flash message saying that a user with email/username already exists
+            flash('A user with that email and/or username already exists.', 'danger')
+            return redirect(url_for('signup'))
+        # If check_user is empty, create a new record in the user table
+        new_user = User(first_name=first_name, last_name=last_name, email=email, username=username, password=password)
+        # Flash a success message
+        flash(f'Thank you {new_user.username} for signing up!', 'success')
+        # Redirect back to Home
         return redirect(url_for('index'))
-    
+
     return render_template('signup.html', form=form)
 
 
